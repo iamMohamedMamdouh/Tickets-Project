c7b5afa4ea282a50958f53f71188ed409451c2e5
Mohamed Mamdouh <161685106+iamMohamedMamdouh@users.noreply.github.com>
2025-05-10 10:10:29 +0300

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,22 +1,3 @@
-//public class Passenger {
-//    private String name;
-//    private String phoneNumber;
-//
-//    public Passenger(String name, String phoneNumber) {
-//        this.name = name;
-//        this.phoneNumber = phoneNumber;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getPhoneNumber() {
-//        return phoneNumber;
-//    }
-//}
-
-
 class Passenger {
         private String name;
         private String phoneNumber;
